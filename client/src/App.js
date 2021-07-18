ff84ee6b844011aaa36a84c897db16c772830d78
Jihani-31 <jihaninanayakkara@gmail.com>
2021-07-19 04:36:42 +0530

    sec add

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import dashboard from './pages/dashboard';
-import cpd from './pages/cpd';
-import course from './pages/course';
-import workshop from './pages/workshop';
-import blogs from './pages/blog';
-import forum from './pages/forum';
-import reports from './pages/reports';
-import job from './pages/job';
-import payments from './pages/payments';
+
+import dashboard from './pages/professional/dashboard';
+import cpd from './pages/professional/course';
+import course from './pages/professional/course';
+import workshop from './pages/professional/workshop';
+import blogs from './pages/professional/blog';
+import forum from './pages/professional/forum';
+import reports from './pages/professional/reports';
+import job from './pages/professional/job';
+import payments from './pages/professional/payments';
 
 
 function App() {
@@ -20,7 +21,7 @@ function App() {
     return (
       <>
         <Router>
-          <Navbar name="jee" type="Cha"/>
+          <Navbar name="jee" type="pro"/>
           <Switch>
             <Route path='/' exact component={dashboard} />
             <Route path='/cpd' component={cpd} />
--- client/src/pages/professional/dashboard.js
+++ client/src/pages/professional/dashboard.js
@@ -1,26 +1,174 @@
 import React from "react";
-import "../pages/style/dashboard.css";
+// import "../pages/style/dashboard.css";
+import "./style/dashboard.css";
+import { makeStyles, Paper, Grid } from "@material-ui/core";
+import { Line, Pie, Doughnut, Bar } from "react-chartjs-2";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    margin: 30,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    height: 200,
+  },
+}));
+
+const state = {
+  labels: ["2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"],
+  datasets: [
+    {
+      label: "Members in CSSL",
+      fill: false,
+      lineTension: 0.5,
+      backgroundColor: "rgba(75,192,192,1)",
+      borderColor: "rgba(0,0,0,1)",
+      borderWidth: 2,
+      data: [65, 59, 80, 81, 56, 70, 80, 40],
+    },
+  ],
+};
+
+const state1 = {
+  labels: [
+    "2012",
+    "2013",
+    "2014",
+    "2015",
+    "2016",
+    "2017",
+    "2018",
+    "2019",
+    "2020",
+  ],
+  datasets: [
+    {
+      label: "Rainfall",
+      backgroundColor: [
+        "#00A6B4",
+        "#00A6B4",
+        "#00A6B4",
+        "#00A6B4",
+        "#00A6B4",
+        "#00A6B4",
+        "#00A6B4",
+        "#00A6B4",
+        "#00A6B4",
+      ],
+      hoverBackgroundColor: [
+        "#003350",
+        "#003350",
+        "#003350",
+        "#003350",
+        "#003350",
+        "#003350",
+        "#003350",
+        "#003350",
+        "#003350",
+      ],
+      data: [
+        40000, 45000, 30000, 20000, 45000, 12345, 2300, 6000, 45000, 23000,
+      ],
+    },
+  ],
+};
+
+const options = {
+  maintainAspectRatio: false,
+};
 
 function Home() {
+  const classes = useStyles();
   return (
     <div className="main">
       <div className="progress">
         <div className="progressBar">
-          <div className="progressColour">
-           
-          </div>
+          <div className="progressColour"></div>
         </div>
       </div>
 
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={12}>
+            <Paper className={classes.paper} elevation={3}>
+              <div className="chart1">
+                <Line
+                  data={state}
+                  width={200}
+                  height={40}
+                  options={
+                    ({ options },
+                    {
+                      title: {
+                        display: true,
+                        text: "Average Rainfall per month",
+                        fontSize: 20,
+                      },
+                      legend: {
+                        display: true,
+                        position: "left",
+                      },
+                    })
+                  }
+                />
+              </div>
+            </Paper>
+          </Grid>
+        </Grid>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Paper className={classes.paper} elevation={3}>
+              <Bar
+                data={state1}
+                width={100}
+                height={40}
+                options={{
+                  title: {
+                    display: true,
+                    text: "Average Rainfall per month",
+                    fontSize: 20,
+                  },
+                  legend: {
+                    display: true,
+                    position: "left",
+                  },
+                }}
+              />
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper} elevation={3}>
+              <Doughnut
+                data={state}
+                options={{
+                  title: {
+                    display: true,
+                    text: "Average Rainfall per month",
+                    fontSize: 20,
+                  },
+                  legend: {
+                    display: true,
+                    position: "left",
+                  },
+                }}
+              />
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+      {/* 
       <div className="Charts">
-        {/* <div className="chart1">chart1</div>
-        <div className="chart2">chart2</div> */}
+        <div className="chart1">chart1</div>
+        <div className="chart2">chart2</div>
       </div>
 
-    <div className="OnGoing">
-    <div className="ONG" >ongoing</div>
+      <div className="OnGoing">
+        <div className="ONG" >ongoing</div>
 
-    </div>
+      </div> */}
     </div>
   );
 }
