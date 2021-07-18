cc1fedc4038be03bf03c418c43b48d3c021b430c
Jihani-31 <jihaninanayakkara@gmail.com>
2021-07-19 04:48:33 +0530

    sec and council

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -315,6 +315,66 @@ function Navbar(props) {
       </>
     );
   } 
+  else if (props.type == "sec") {
+    return (
+      <>
+        <div className="buttons">
+          <a href="#" className="Logout">
+            Logout
+          </a>
+          <div className="panal">
+            <Link to="#" className="notification">
+              <MdIcons.MdNotifications />
+            </Link>
+
+            <Link to="#" className="settings">
+              <AiIcons.AiFillSetting />
+            </Link>
+          </div>
+        </div>
+
+        <IconContext.Provider value={{ color: "#fff" }}>
+          <div className="navbar">
+            <Link to="#" className="menu-bars">
+              <FaIcons.FaBars onClick={showSidebar} />
+            </Link>
+          </div>
+          <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+            <div className="navbar">
+              <Link to="#" className="menu-bars-collaps">
+                <FaIcons.FaBars onClick={showSidebar} />
+              </Link>
+            </div>
+            <ul className="nav-menu-items" onClick={showSidebar}>
+              <li className="navbar-toggle">
+                <div className="profile">
+                  <div className="profileImg">
+                    <img src={progileImg} className="pic"></img>
+                  </div>
+                  <div className="profileDetails">
+                    <h2>{props.name}</h2>
+
+                    <p>{props.type}</p>
+                  </div>
+                </div>
+              </li>
+
+              {SidebarDataCha.map((item, index) => {
+                return (
+                  <li key={index} className={item.cName}>
+                    <Link to={item.path}>
+                      {item.icon}
+                      <span>{item.title}</span>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </IconContext.Provider>
+      </>
+    );
+  } 
 
 }
 
---
+++ client/src/components/SidebarDataSec.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+import * as SiIcons from  "react-icons/si";
+import * as GIIcons from  "react-icons/gi";
+import * as HiIcons from  "react-icons/hi";
+import * as MdIcons from  "react-icons/md";
+export const SidebarDataSec = [
+  {
+    title: 'Dashboard',
+    path: '/',
+    icon: <AiIcons.AiFillDashboard />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Course',
+    path: '/course',
+    icon: <FaIcons.FaGraduationCap />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Workshops',
+    path: '/workshop',
+    icon: <GIIcons.GiTeacher />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Blogs',
+    path: '/blog',
+    icon: <FaIcons.FaBloggerB />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Forum',
+    path: '/forum',
+    icon: <MdIcons.MdForum />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Reports',
+    path: '/reports',
+    icon: <AiIcons.AiFillFileText />,
+    cName: 'nav-text'
+  },{
+    title: 'Payments',
+    path: '/payments',
+    icon: <AiIcons.AiFillDollarCircle />,
+    cName: 'nav-text'
+  },
+];
