e6879fb102049abd0fe3d628980a3a003f0086bd
Jihani-31 <jihaninanayakkara@gmail.com>
2021-07-20 16:04:51 +0530

    reg approve

--- client/src/pages/secretary/regApprove.js
+++ client/src/pages/secretary/regApprove.js
@@ -1,11 +1,47 @@
 import React from "react";
 import "./style/regApprove.css";
+import data from './data';
+import Card from "@material-ui/core/Card";
+import DataTable from 'react-data-table-component';
+
+
+const columns = [
+  {
+    name: 'Title',
+    selector: 'title',
+    sortable: true,
+  },
+  {
+    name: 'Year',
+    selector: 'year',
+    sortable: true,
+    right: true,
+  },
+];
+
 function regApprove() {
   return (
-    <div className="main">
-    
-     
-    </div>
+    <>
+      <div className="main">
+        
+        <div className="mainCourses">
+          <div className="course">
+           
+            <div className="courseDes">
+        
+            <DataTable
+              title="Arnold Movies"
+              columns={columns}
+              data={data}
+              pagination
+
+          />
+      
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
