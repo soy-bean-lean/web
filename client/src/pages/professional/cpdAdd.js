415e50289a238da2ee233219675b60c2c0b9685f
Chamika Deshan Jayasingha <56592121+ChamikaD97@users.noreply.github.com>
2021-07-21 20:40:33 +0530

    create cpdAdd JS

---
+++ client/src/pages/professional/cpdAdd.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./style/forum.css";
+function addCPD(){
+    return(
+        <div className='title'>
+        <h1>Add CPD</h1>
+      </div>
+    );
+}
+
+export default addCPD;
