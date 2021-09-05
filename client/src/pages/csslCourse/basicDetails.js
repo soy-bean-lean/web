4ac2ba3474c1c2014908bb49e106a6115ad21b06
Supun Nanayakkara <supunnanayakkara11@gmail.com>
2021-09-05 14:10:32 +0530

    submit course details and redirect to course content page

--- client/src/pages/csslCourse/basicDetails.js
+++ client/src/pages/csslCourse/basicDetails.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./basicDetails.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 function BasicCourseInfo() {
   const [courseTitle, setCourseTitle] = useState("");
@@ -11,38 +11,43 @@ function BasicCourseInfo() {
   const [mode, setMode] = useState("");
   const [imgFile, setImgFile] = useState();
 
-  const [uploadStatus,setUploadStatus] = useState("");
+  const [uploadStatus, setUploadStatus] = useState("");
 
-  const insertCourseInfo = () => {
+  let history = useHistory();
+
+  const InsertCourseInfo = () => {
     //const file = event.target.files[0];
     const mId = "cssl001";
     const formData = new FormData();
-    formData.append('image', imgFile);
-    formData.append('title',courseTitle);
-    formData.append('description',courseDes);
-    formData.append('duration',courseDuration);
-    formData.append('language',language);
-    formData.append('level',level);
-    formData.append('mode',mode);
-    formData.append('lecturer',mId);
+    formData.append("image", imgFile);
+    formData.append("title", courseTitle);
+    formData.append("description", courseDes);
+    formData.append("duration", courseDuration);
+    formData.append("language", language);
+    formData.append("level", level);
+    formData.append("mode", mode);
+    formData.append("lecturer", mId);
 
-    fetch('http://localhost:3001/csslcourse/basicInfo',{
+    fetch("http://localhost:3001/csslcourse/basicInfo", {
       method: "POST",
       body: formData,
-      headers:{
-        'Accept': 'multipart/form-data',
+      headers: {
+        Accept: "multipart/form-data",
       },
       credentials: "include",
     })
-    .then(res => res.json())
-    .then(res => {
-      setUploadStatus(res.msg);
-      alert(uploadStatus);
-    })
-    .catch(error => {
-      console.log(error);
-    })
-  }
+      .then((res) => res.json())
+      .then((res) => {
+        setUploadStatus(res.msg);
+        alert("Successful");
+        let path = '/addcourseContent';
+        history.push(path);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log("Run using button");
+  };
 
   return (
     <>
@@ -53,14 +58,18 @@ function BasicCourseInfo() {
           <div className="course-basic-info-block">
             <div className="course-field-block">
               <h4 className="course-info-title">Name</h4>
-              <input className="input" placeholder="--Course Title--" onChange={(e)=>setCourseTitle(e.target.value)}></input>
+              <input
+                className="input"
+                placeholder="--Course Title--"
+                onChange={(e) => setCourseTitle(e.target.value)}
+              ></input>
             </div>
             <div className="course-field-block">
               <h4 className="course-info-title">Description</h4>
               <input
                 className="input"
                 placeholder="--Course Description--"
-                onChange={(e)=>setCourseDes(e.target.value)}
+                onChange={(e) => setCourseDes(e.target.value)}
               ></input>
             </div>
             <div className="course-field-block">
@@ -68,12 +77,16 @@ function BasicCourseInfo() {
               <input
                 className="input"
                 placeholder="--Approximate Duration--"
-                onChange={(e)=>setCourseDuration(e.target.value)}
+                onChange={(e) => setCourseDuration(e.target.value)}
               ></input>
             </div>
             <div className="course-field-block">
               <h4 className="course-info-title">Language</h4>
-              <select name="select" id="course-language" onChange={(e)=>setLanguage(e.target.value)}>
+              <select
+                name="select"
+                id="course-language"
+                onChange={(e) => setLanguage(e.target.value)}
+              >
                 <option value="type">--Select Language--</option>
                 <option value="English">English</option>
                 <option value="Sinhala">Sinhala</option>
@@ -82,7 +95,11 @@ function BasicCourseInfo() {
             </div>
             <div className="course-field-block">
               <h4 className="course-info-title">Level</h4>
-              <select name="select" id="course-level" onChange={(e)=>setLevel(e.target.value)}>
+              <select
+                name="select"
+                id="course-level"
+                onChange={(e) => setLevel(e.target.value)}
+              >
                 <option value="type">--Select Level--</option>
                 <option value="Beginner">Beginner</option>
                 <option value="Intermediate">Intermediate</option>
@@ -91,7 +108,11 @@ function BasicCourseInfo() {
             </div>
             <div className="course-field-block">
               <h4 className="course-info-title">Mode</h4>
-              <select name="select" id="course-mode" onChange={(e)=>setMode(e.target.value)}>
+              <select
+                name="select"
+                id="course-mode"
+                onChange={(e) => setMode(e.target.value)}
+              >
                 <option value="type">--Select Mode--</option>
                 <option value="Online">Online</option>
                 <option value="Offline">Offline</option>
@@ -111,16 +132,24 @@ function BasicCourseInfo() {
           </div>
           <div className="course-btn-block">
             <input
-                            type="submit"
-                            className="course-btn-submit"
-                            value="Submit"
-                            onClick={insertCourseInfo}
-                        />
-            {/*<Link to={"/addcourseContent/"} className="course-btn-submit" onClick={display}>
-              <a href="#" className="course-btn-submit">
-                Submit
-              </a>
-  </Link>*/}
+              type="submit"
+              className="course-btn-submit"
+              value="Submit"
+              onClick={InsertCourseInfo}
+            />
+            {
+              
+                /*<Link
+                to={"/addcourseContent/"}
+                className="course-btn-submit"
+                onClick={InsertCourseInfo}
+              >
+                <a href="#" className="course-btn-submit">
+                  Submit
+                </a>
+              </Link>*/
+              
+            }
           </div>
         </div>
       </div>
