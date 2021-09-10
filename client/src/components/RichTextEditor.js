1b65ac9fa2b4f4b8624858dd01efa81a72f525cd
Supun Nanayakkara <supunnanayakkara11@gmail.com>
2021-09-11 05:05:36 +0530

    change client/cpdAdd.js and server/cpdRecord.js

--- client/src/components/RichTextEditor.js
+++ client/src/components/RichTextEditor.js
@@ -185,39 +185,3 @@ const TextEditor = (props) => {
   )
 }
 export default TextEditor;
-
-/*function TextEditor(props) {
-    const [editorState, setEditorState] = useState(() =>
-      EditorState.createEmpty()
-    );
-    const [convertedContent, setConvertedContent] = useState(null);
-    const onEditorStateChange = (state) => {
-      setEditorState(state);
-      //convertContentToHTML();
-    };
-    
-    const convertContentToHTML = () => {
-      let currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
-      setConvertedContent(currentContentAsHTML);
-      props.onValueChange(convertedContent);
-    };
-  
-    const createMarkup = (html) => {
-      return {
-        __html: DOMPurify.sanitize(html),
-      };
-    };
-    return (
-      <div className="text-editor">
-        <Editor
-          editorState={editorState}
-          toolbarClassName="toolbarClassName"
-          wrapperClassName="wrapperClassName"
-          editorClassName="editorClassName"
-          onEditorStateChange={onEditorStateChange}
-        />
-        {/*<div className="preview" dangerouslySetInnerHTML={createMarkup(convertedContent)}></div>}
-      </div>
-    );
-  }
-  export default TextEditor;*/
