e062ea21eace94267565ddc16f67cd8c2c1957ef
Jihani-31 <jihaninanayakkara@gmail.com>
2021-09-22 22:24:11 +0530

    Change 1.1

--- clientside/src/pages/jobs/addJobCv.js
+++ clientside/src/pages/jobs/addJobCv.js
@@ -74,13 +74,13 @@ const AddCV = () => {
     if (result == 'err') {
       return (
         <>
-          <Alert color="danger">Unsuccefull Attempt,Try Againg</Alert>
+          <Alert color="danger">Unsuccessfull Attempt,Try Again</Alert>
         </>
       );
     } else if (result == 'done') {
       return (
         <>
-          <Alert color="success">Greate Attempt is Succesfull</Alert>
+          <Alert color="success">Greate Attempt is Successfull</Alert>
         </>
       );
     }
